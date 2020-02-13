b664332d0a4a92f12bdb447747ee7fb73f1ce5ff
ofnullable <ofnullable@gmail.com>
2020-02-14 02:14:42 +0900

    :lipstick: Add layout components for common view

---
+++ src/layout/Footer.svelte
@@ -0,0 +1,5 @@
+<style>
+
+</style>
+
+<footer>&copy;</footer>
---
+++ src/layout/Layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from './Header';
+  import Footer from './Footer';
+</script>
+
+<style>
+
+</style>
+
+<Header />
+<main>
+  <slot />
+</main>
+<Footer />
