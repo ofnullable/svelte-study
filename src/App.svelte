626e070cd491213fbcd3914a6791861432678fc4
ofnullable <ofnullable@gmail.com>
2020-03-27 13:47:09 +0900

    :sparkles: Add token symbol validation in router

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<Router {routes} />
+<Router {routes} on:conditionsFailed="{routeFailed}" />
 
 <script>
   import Router, { replace, wrap } from 'svelte-spa-router';
@@ -6,15 +6,33 @@
   import Error from './pages/Error';
   import DashBoard from './pages/DashBoard';
 
+  import { token } from './store/token';
+  import { getToken } from './common/utils';
+
   import 'normalize.css';
   import './App.scss';
 
   const routes = {
     '/': wrap(Trade, _ => {
       replace('/AC3');
+      return true;
     }),
     '/dashboard': DashBoard,
-    '/:symbol': Trade,
+    '/:symbol': wrap(Trade, detail => {
+      const symbol = detail.location.replace(/\//, '');
+      const currToken = symbol ? getToken(symbol) : 'AC3';
+      if (currToken) {
+        $token = currToken;
+        return true;
+      }
+      return false;
+    }),
+    '/error/:status': Error,
     '*': Error,
+  };
+
+  const routeFailed = e => {
+    console.error(e);
+    replace('/error/404');
   };
 </script>
--- src/pages/Trade.svelte
+++ src/pages/Trade.svelte
@@ -4,22 +4,7 @@
 </Layout>
 
 <script>
-  import { onMount, onDestroy, beforeUpdate } from 'svelte';
   import Layout from '../layouts/Layout';
   import TokenList from '../components/tokens/TokenList';
   import TokenInfo from '../components/tokens/TokenInfo';
-  import { getToken } from '../common/utils';
-  import { token } from '../store/token';
-
-  export let params = {};
-
-  onMount(() => {
-    console.log($token);
-  });
-  onDestroy(() => {
-    console.log('destroy');
-  });
-  beforeUpdate(() => {
-    $token = getToken(params.symbol);
-  });
 </script>
